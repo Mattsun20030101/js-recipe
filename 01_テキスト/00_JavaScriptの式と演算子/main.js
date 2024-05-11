98cafdaf6f3e6b02f039e533dfb8c4a401a09bf9
Mattsun20030101 <arashi5lovers@gmail.com>
2024-05-11 13:50:31 +1000

    ３の倍数を学習しました

--- 01_テキスト/00_JavaScriptの式と演算子/main.js
+++ 01_テキスト/00_JavaScriptの式と演算子/main.js
@@ -1,9 +1,23 @@
 // TRY: 練習問題１
+let x = 57052 + 57054
+console.log(x)
+
+let y = "114" + "106"
+console.log(y)
 
 // TRY: 練習問題２
+let z = 5
+z += 2
+z *= 5
+z = 100 % z
+z /= 6
+
+console.log(z)
+
+
 
 // TRY: 練習問題３
-let happiness = 0 // 0 から 100 までの数値を代入してください
+let happiness = 0// 0 から 100 までの数値を代入してください
 let sleepiness = 0 // 0 から 100 までの数値を代入してください
 
 const isHappy = happiness > 90
@@ -11,7 +25,10 @@ const isSleepy = sleepiness > 90
 
 const isNotHappyAndSleepy = !isHappy && isSleepy
 
+
 // 以下のコメントアウトを外して確認してみましょう
-// console.log("isNotHappyAndSleepy" + "は" + true + "ですか？", isNotHappyAndSleepy)
-// console.log("isHappy", isHappy)
-// console.log("isSleepy", isSleepy)
+ console.log("isNotHappyAndSleepy" + "は" + true + "ですか？", isNotHappyAndSleepy)
+ console.log("isHappy", isHappy)
+ console.log("isSleepy", isSleepy)
+ 
+
